07269ccdf8a3e9f7003259079b842f0974c2f9ae
yu zhou <yu.zhou@razer.com>
2021-12-07 18:07:24 +0800

    添加轮播器

---
+++ lib/ui/home/home_carousel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeCarousel extends StatefulWidget {
+  const HomeCarousel({Key? key}) : super(key: key);
+
+  @override
+  State<HomeCarousel> createState() => _HomeCarousel();
+}
+
+class _HomeCarousel extends State<HomeCarousel> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("test");
+  }
+}
