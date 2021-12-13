d014db38b708530b5278eca101f35efdca6f0329
yu zhou <yu.zhou@razer.com>
2021-12-13 15:07:26 +0800

    添加detail 页面

---
+++ lib/ui/detail_page/detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key}): super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
