562ade50f0eab73a8325214fa45be3c5970ef86a
yu zhou <yu.zhou@razer.com>
2021-12-13 10:14:51 +0800

    修改轮播layout

--- lib/ui/home/home_carousel_tile.dart
+++ lib/ui/home/home_carousel_tile.dart
@@ -12,18 +12,22 @@ class HomeCarouselTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Image(
-          image: NetworkImage(topNewsModel.image),
-          fit: BoxFit.fill,
-          loadingBuilder: (BuildContext context, Widget child,
-                  ImageChunkEvent? loadingProgress) =>
-              loadingProgress == null
-                  ? child
-                  : const SizedBox(
-                      height: double.infinity,
-                      width: double.infinity,
-                      child: ColoredBox(color: Colors.grey),
-                    ),
+        SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Image(
+            image: NetworkImage(topNewsModel.image),
+            fit: BoxFit.fill,
+            loadingBuilder: (BuildContext context, Widget child,
+                ImageChunkEvent? loadingProgress) =>
+            loadingProgress == null
+                ? child
+                : const SizedBox(
+              height: double.infinity,
+              width: double.infinity,
+              child: ColoredBox(color: Colors.grey),
+            ),
+          ),
         ),
         Positioned(
             left: 30,
